f9484d9a30f257c3d4d81c21eedc63847e12c148
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-08 09:56:01 +0300

    adding code again.

--- 10. Exercise Single Page Applications/Exercise_SinglePageApplication/01.Forum/details.js
+++ 10. Exercise Single Page Applications/Exercise_SinglePageApplication/01.Forum/details.js
@@ -22,7 +22,7 @@ async function loadPost(postId) {
         if (!res.ok) {
             const err = await res.json();
             throw new Error(err.message);
-        } 
+        }
         const post = await res.json();
         const themeContentDivElement = document.querySelector('.theme-content');
         themeContentDivElement.replaceChildren();
@@ -38,7 +38,60 @@ async function loadPost(postId) {
         })
         createElements('h2', post.title, themeNameDivElement, {})
 
-    } catch (error) {
+        const commentDivElement = createElements('div', '', themeContentDivElement, {
+            'class': 'comment'
+        })
+        const headerDivElement = createElements('div', '', commentDivElement, {
+            'class': 'header'
+        })
+        createElements("img", "", headerDivElement, {
+            src: "./static/profile.png", alt: "avatar"
+        });
+        const paragraphElement = createElements("p", "", headerDivElement, {});
+        paragraphElement.innerHTML = `<span>${post.username}</span> posted on <time>${post.createdDate}</time>`;
+        createElements('p', post.content, headerDivElement, {
+            class: 'post-content'
+        });
+        const comments = await loadComments(postId)
 
+        for (const comment of Object.values(comments)) {
+            const userCommentDivElement = createElements("div", "", commentDivElement, {
+                class: 'user-comment'
+            })
+            const topicNameWrapper = createElements('div', '', userCommentDivElement, {
+                class: 'topic-name-wrapper'
+            })
+            const topicNameDivElement = createElements('div', '', topicNameWrapper, {
+                class: 'topic-name'
+            })
+            const paragraphElement = createElements('p', '', topicNameDivElement,{} )
+            paragraphElement.innerHTML = `<strong>${comment.username}</strong> commented on <time>${comment.createDate}</time>`
+            const postContentDivElement = createElements('div', '', topicNameDivElement, {
+                class: 'post-content'
+            })
+            createElements('p', comment.content, postContentDivElement, {})
+        }
+        const answerCommentDivElement = createElements('div', '', themeContentDivElement, {
+            class: 'answer-comment'
+        })
+        const answerParagraphElement = createElements('p', '', answerCommentDivElement, {})
+        answerCommentDivElement.innerHTML = `<span>CurrentUser</span> comment:`
+        
+    } catch (error) {
+        alert(error.message)
+    }
+}
+
+async function loadComments(postId) {
+    try {
+        const res = await fetch('http://localhost:3030/jsonstore/collections/myboard/comments');
+        if (!res.ok) {
+            const err = await res.json();
+            throw new Error(err.message)
+        }
+        const comments = await res.json();
+        return Object.values(comments).filter(comment => comment.postId === postId);
+    } catch (error) {
+        alert(error.message);
     }
 }
