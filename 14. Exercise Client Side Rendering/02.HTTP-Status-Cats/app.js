a102869e37d9bfb17a07a4f6be0474ac0216ff10
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-16 13:34:18 +0300

    add new code to app.js

--- 14. Exercise Client Side Rendering/02.HTTP-Status-Cats/app.js
+++ 14. Exercise Client Side Rendering/02.HTTP-Status-Cats/app.js
@@ -1,4 +1,5 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { styleMap } from '../node_modules/lit-html/directives/style-map.js';
 import { cats } from './catSeeder.js';
 
 const allCats = document.getElementById('allCats');
@@ -7,35 +8,37 @@ allCats.addEventListener('click', toggle);
 
 const cardTemplate = (data) => html`
 <ul>
-
-        <li>
-                <img src="./images/cat100.jpg" width="250" height="250" alt="Card image cap">
-                <div class="info">
-                    <button class="showBtn">Show status code</button>
-                    <div class="status" style="display: none" id="100">
-                        <h4>Status Code: 100</h4>
-                        <p>Continue</p>
-                    </div>
-                </div>
-            </li>
-
+    ${data.map(cat =>html`
+    <li>
+        <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
+        <div class="info">
+            <button class="showBtn">${cat.info ? 'Hide' : 'Show'} status code</button>
+            <div class="status" style=${styleMap(cat.info ? {display:'block'} : {display:'none'})} id="${cat.id}">
+                <h4>Status Code: ${cat.statusCode}</h4>
+                <p>${cat.statusMessage}</p>
+            </div>
+        </div>
+    </li>
+    `)}
 </ul>
 `
 
 
-
-
-cats.forEach(c => console.log(c));
+// we hae to walk around data 
+cats.forEach(c => c.info = false);
 update();
 
 function update() {
     const result = cardTemplate(cats);
     render(result, allCats); // we send result to continer allcats
 
-
 }
 
-function toggle() {
-    console.log('test');
+function toggle(event) {
+    const elementId = event.target.parentNode.querySelector('.status').id;
+    const cat = cats.find(c => c.id == elementId);
+    
+    cat.info = !cat.info;
+    update();
 
 }
