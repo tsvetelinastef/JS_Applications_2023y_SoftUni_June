0d67d8e3d2b11d0d38bd33591af34be6e903eeb8
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-18 14:44:05 +0300

    Update search.js

--- 14. Exercise Client Side Rendering/03.Search-in-List/search.js
+++ 14. Exercise Client Side Rendering/03.Search-in-List/search.js
@@ -35,8 +35,7 @@ function search() {
 
 function countMatches(towns, match) {
    const matches = towns.filter(t => match && t.toLowerCase().includes(match.toLowerCase())).length;
-   return matches ? `${matches} matches found` : '';
-   
+   return `${matches} matches found`;  //  return matches ? `${matches} matches found` : '';
 }
 
 
