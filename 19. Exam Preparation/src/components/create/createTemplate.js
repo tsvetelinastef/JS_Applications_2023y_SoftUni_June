93ef10a4490476ad30ac03ae7bcfffa5f0db3d59
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-23 01:00:33 +0300

    Update createTemplate.js

---
+++ 19. Exam Preparation/src/components/create/createTemplate.js
@@ -0,0 +1,48 @@
+import { html } from "lit-html";
+
+export const createTemplate = (submitHandler) => html`
+    <section id="create">
+        <div class="form">
+            <h2>Add item</h2>
+            <form class="create-form" @submit=${submitHandler}>
+                <input
+                    type="text"
+                    name="brand"
+                    id="shoe-brand"
+                    placeholder="Brand"
+                />
+                <input
+                    type="text"
+                    name="model"
+                    id="shoe-model"
+                    placeholder="Model"
+                />
+                <input
+                    type="text"
+                    name="imageUrl"
+                    id="shoe-img"
+                    placeholder="Image url"
+                />
+                <input
+                    type="text"
+                    name="release"
+                    id="shoe-release"
+                    placeholder="Release date"
+                />
+                <input
+                    type="text"
+                    name="designer"
+                    id="shoe-designer"
+                    placeholder="Designer"
+                />
+                <input
+                    type="text"
+                    name="value"
+                    id="shoe-value"
+                    placeholder="Value"
+                />
+
+                <button type="submit">post</button>
+            </form>
+        </div>
+    </section>`;
