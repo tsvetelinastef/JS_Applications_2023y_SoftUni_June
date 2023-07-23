15045841d809fe202c573c15ae19db2aebb33e07
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-23 22:53:05 +0300

    Update create.js ExamPrep pt. 1

--- 19. Exam Preparation/src/components/create/create.js
+++ 19. Exam Preparation/src/components/create/create.js
@@ -5,16 +5,36 @@ export class CreateComponent {
         this.templateFunction = templateFunction;
         this.router = router;
         this.showView = this._showView.bind(this);
+        this.createHandler = this._createHandler.bind(this);
     }
 
-    async _showView() {
-        let shoes = await this.shoeService.getAll();
-        let template = this.templateFunction(shoes);
+    async _showView() {     // let shoes = await this.shoeService.getAll();
+        let template = this.templateFunction(this.createHandler);
         this.renderHandler(template);
     }
 
     async _createHandler(e) {
+        e.preventDefault();
+        let form = e.target;
+        let formData = new FormData(form);
 
+      let shoe =  {
+            brand: formData.get,
+            model,
+            imageUrl,
+            release,
+            designer,
+            value
+        }
+
+
+        let email = formData.get('email');
+        let password = formData.get('password');
+        
+        if(email == '' || password == '' ) {
+            alert('Email and Password must not be empty');
+            return;
+        }
     }
 }
 
