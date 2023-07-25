0e53617e5305182ccdf802e58f96a11440bf9337
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-26 00:48:06 +0300

    update 19. examPrep pt.1

--- 19. Exam Preparation/src/components/search/search.js
+++ 19. Exam Preparation/src/components/search/search.js
@@ -4,11 +4,21 @@ export class SearchComponent {
         this.renderHandler = renderHandler;
         this.templateFunction = templateFunction;
         this.showView = this._showView.bind(this);
+        this.searchHandler = this._searchHandler.bind(this);
     }
 
-    async _showView() {
-        let shoes = await this.shoeService.getAll();
+    async _showView(ctx) {
+        // let shoes = await this.shoeService.getAll();
+        let queryString = ctx.querystring;
+        let queryArr = queryString.split('='); 
+        let value = queryArr[1];
         let template = this.templateFunction(shoes);
         this.renderHandler(template);
+    }
+
+    async _searchHandler(e) {
+        e.preventDefault();
+        let form = e.target;
+        let formData = new FormData(form);
     }
 }
