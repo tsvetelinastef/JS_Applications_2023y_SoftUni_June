b9f4db47fbb258da567ea33a3873e10b558befc9
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-27 17:09:40 +0300

    update folder 20. ExamPrep_Feb_2022

---
+++ 20. ExamPrep_Feb_2022y/src/api/api.js
@@ -0,0 +1,29 @@
+import { clearUserData, getAccessToken } from '../util.js';
+
+const host = 'http://localhost:3030';
+
+async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {}
+    };
+
+    const token = getAccessToken();
+    if(token) {
+        options.headers['X-Authorization'] = token;
+    }
+
+    if(data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    try {
+        const response = await fetch(host + url, options);
+
+        if(response.ok != true) {
+            
+        }
+    }
+
+}
