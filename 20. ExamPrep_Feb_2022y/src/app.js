5e67552271866d6e0380c5835cbd2e12c8bd325c
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-28 00:11:49 +0300

    update folder 20. ExamPrep_Feb_2022

--- 20. ExamPrep_Feb_2022y/src/app.js
+++ 20. ExamPrep_Feb_2022y/src/app.js
@@ -1,8 +1,16 @@
 // we create file "api.js" and we load it in "index.html" and after this  we add this: 
 // alert('it works'); - after that open the browser and press the button F5, because it can't do it automatically 
 
-import page from '../node_modules/page/page.mjs';
+import page from '../node_modules/page/page.mjs'; // 1st thing we need is page module, because it navigates and ties up our entire application, writes functions our dependency injections  
 
 page('/', () => console.log('home page')); // for the staring view - console.log
+// in the Inspector and in the browser ->  we see: "home page" 
+
+page('/catalog', () => console.log('catalog page'));
+page('/login', () => console.log('login page'));
+page('/register', () => console.log('register page'));
+page('/create', () => console.log('create page'));
+page('/details/:id', () => console.log('details page'));
+page('/edit/:id', () => console.log('details page'));
 
 page.start();
