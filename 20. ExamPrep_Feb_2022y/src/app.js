bcfb1d390ba2b739c6b013ac72ada01fa01cf85a
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-27 15:03:30 +0300

    update folder 20. ExamPrep_Feb_2022

--- 20. ExamPrep_Feb_2022y/src/app.js
+++ 20. ExamPrep_Feb_2022y/src/app.js
@@ -1,2 +1,7 @@
-// alert('it works'); - after pressing the button F5
+// alert('it works'); - after that open the browser and press the button F5, because it can't do it automatically 
 
+import page from '../node_modules/page/page.mjs';
+
+page('/', () => console.log('home page')); // for the staring view - console.log
+
+page.start();
