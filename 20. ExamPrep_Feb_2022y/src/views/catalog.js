13f9b76ce373de8242b5fe4ee2fdd8ef299af3d3
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-29 00:06:38 +0300

    update folder 20. ExamPrep_Feb_2022

--- 20. ExamPrep_Feb_2022y/src/views/catalog.js
+++ 20. ExamPrep_Feb_2022y/src/views/catalog.js
@@ -1,40 +1,30 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
-const catalogTemplate = () => html`
+import * as gamesService from '../api/games.js';
+
+
+const catalogTemplate = (games) => html`
 <section id="catalog-page">
     <h1>All Games</h1>
-    <!-- Display div: with information about every game (if any) -->
-    <div class="allGames">
-        <div class="allGames-info">
-            <img src="./images/avatar-1.jpg">
-            <h6>Action</h6>
-            <h2>Cover Fire</h2>
-            <a href="#" class="details-button">Details</a>
-        </div>
-
-    </div>
-    <div class="allGames">
-        <div class="allGames-info">
-            <img src="./images/avatar-1.jpg">
-            <h6>Action</h6>
-            <h2>Zombie lang</h2>
-            <a href="#" class="details-button">Details</a>
-        </div>
-
-    </div>
-    <div class="allGames">
-        <div class="allGames-info">
-            <img src="./images/avatar-1.jpg">
-            <h6>Action</h6>
-            <h2>MineCraft</h2>
-            <a href="#" class="details-button">Details</a>
-        </div>
-    </div>
-
-    <!-- Display paragraph: If there is no games  -->
-    <h3 class="no-articles">No articles yet</h3>
+   
+    ${games.length > 0
+        ? games.map(cardTemplate) 
+        : html`<h3 class="no-articles">No articles yet</h3>` 
+    }
+ 
 </section> `;
 
-export function catalogPage(ctx) {
-    ctx.render(catalogTemplate());
+const cardTemplate = (game) => html`
+<div class="allGames">
+    <div class="allGames-info">
+        <img src=${game.imageUrl}>
+        <h6>${game.category}</h6>
+        <h2>${game.title}</h2>
+        <a href="/details/${game._id}" class="details-button">Details</a>
+    </div>
+</div>`;
+
+export async function catalogPage(ctx) {
+    const games = await gamesService.getAll();
+    ctx.render(catalogTemplate(games));
 }
