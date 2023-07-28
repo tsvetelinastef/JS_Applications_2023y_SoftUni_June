66b46e96ddee6995c67f71b61207b48fbaa13705
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-07-28 23:15:38 +0300

    home.js is done
    update from folder 20. ExamPrep_Feb_2022

--- 20. ExamPrep_Feb_2022y/src/views/home.js
+++ 20. ExamPrep_Feb_2022y/src/views/home.js
@@ -15,28 +15,27 @@ const homeTemplate = (games) => html`
     <div id="home-page">
         <h1>Latest Games</h1>
 
-        ${games.map(previewTemplate)}
-
-        <!-- Display paragraph: If there is no games  -->
-        <p class="no-articles">No games yet</p>
+        ${games.length > 0 
+            ? games.map(previewTemplate)
+            : html`<p class="no-articles">No games yet</p>`
+        }
+        
     </div>
-</section>`;
-
+</section>`; 
 
 const previewTemplate = (game) => html`  
 <div class="game">
     <div class="image-wrap">
-        <img src="./images/CoverFire.png">
+        <img src=${game.imageUrl}>
     </div>
-    <h3>Cover Fire</h3>
+    <h3>${game.title}</h3>
     <div class="rating">
         <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
     </div>
     <div class="data-buttons">
-        <a href="#" class="btn details-btn">Details</a>
+        <a href="/details/${game._id}" class="btn details-btn">Details</a>
     </div>
 </div>`;
-
 
 export async function homePge(ctx) {
     const games = await gamesService.getRecent();
